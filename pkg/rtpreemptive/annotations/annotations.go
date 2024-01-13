99fe316316e6c4a1cd14a71996958f98ca8078de
LIU XIN <lewisxin718@gmail.com>
2024-01-13 16:18:50 +0100

    chore: adjust atlas metrics annotation key value

--- pkg/rtpreemptive/annotations/annotations.go
+++ pkg/rtpreemptive/annotations/annotations.go
@@ -7,12 +7,12 @@ const (
 	AnnotationKeyDDL = AnnotationKeyPrefix + "ddl"
 	// AnnotationKeyExecTime represents the estimated execution time of a program
 	AnnotationKeyExecTime = AnnotationKeyPrefix + "exec-time"
-	// AnnotationKeyATLASEnabled represents additional metrics of the pod
-	AnnotationKeyATLASMetrics = AnnotationKeyPrefix + "metrics"
 	// AnnotationKeyPausePod represents whether or not a pod is marked to be paused
 	AnnotationKeyPausePod = AnnotationKeyPrefix + "pause-pod"
 	// AnnotationKeyPreemptible indicates a pod cannot be paused
 	AnnotationKeyPreemptible = AnnotationKeyPrefix + "preemptible"
 	// AnnotationKeyATLASEnabled indicates if execution time should be estimated using ATLAS predictor
 	AnnotationKeyATLASEnabled = AnnotationKeyPrefix + "atlas-enabled"
+	// AnnotationKeyATLASMetrics represents additional metrics of the pod
+	AnnotationKeyATLASMetrics = AnnotationKeyPrefix + "atlas-metrics"
 )
